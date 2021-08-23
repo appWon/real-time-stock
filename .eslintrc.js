d5d2498e1d61e4c41a25cc667cb202ac69b71e15
jaewon <korkrip11@gmail.com>
2021-08-23 17:35:55 +0900

    setting

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    semi: 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    'react/prop-types': 'off',
+  },
+};
